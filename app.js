2b02473c33cf7cf57d210ea7ccc8e1effc4b3117
zzzdks760 <zzzdks760@naver.com>
2023-11-24 16:05:24 +0900

    sungmin

--- app.js
+++ app.js
@@ -52,3 +52,27 @@ const changePointer = () => {
 
 /**Event */
 changePointerBtn.addEventListener('click', changePointer);
+/**
+ * 페이지 시작할 때 한번 배경이미지 랜덤으로 바꾸기
+**/
+
+const images= new Array();
+images[0]="images/block/block1.jpg";
+images[1]="images/block/block2.jpg";
+images[2]="images/block/block3.jpg";
+images[3]="images/glass/code1.png";
+images[4]="images/glass/code2.jpg";
+images[5]="images/glass/glass1.jpg";
+images[6]="images/glass/glass2.jpg";
+images[7]="images/glass/glass3.jpg";
+
+
+console.log(images);
+
+function showImage() {
+    let imgNum = Math.floor(Math.random()*images.length);
+    const objImg = document.getElementById("introImg");
+    objImg.src = images[imgNum];
+}
+
+showImage();
