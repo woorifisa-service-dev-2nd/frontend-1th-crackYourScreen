f4d37216ad5eb3f0aa7bbf54246fc29751b1076e
mimimya <sj.ella1030@gmail.com>
2023-11-24 15:33:58 +0900

    Hammer mouse pointer, Hammer Change button Complete

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 /**DOM */
 const changePointerBtn = document.getElementById('change-pointer');
-const htmlBody = document.getElementsByTagName('body')[0];
-const mousPointer = window.getComputedStyle(htmlBody).cursor;
 /**마우스 호버 CSS 초기화 */
 document.styleSheets[0].insertRule('html,button:hover{cursor : url(mouse-pointer/simple-hammer.png), pointer}', 0);
 //console.log(document.styleSheets[0].cssRules[0]);
@@ -13,16 +11,18 @@ document.styleSheets[0].insertRule('html,button:hover{cursor : url(mouse-pointer
  * @returns {void} CSS객체를 수정해 마우스 커서를 바꿈
  * @author 'mimimya'
  */
-let changePointerCounter = 0;
+let changePointerCounter = 1;
 const changePointer = () => {
     /**0.CSS value */
     const pointerNameList = [
-        "url(mouse-pointer/simple-hammer.png), pointer",
-        "url(mouse-pointer/yellow-hammer.png), pointer",
-    ]
+        { url: `url(mouse-pointer/simple-hammer.png), pointer`, deg: `rotate(deg: 0)` },
+        { url: `url(mouse-pointer/real-hammer.png), pointer`, deg: `rotate(deg: 25)` },
+        { url: `url(mouse-pointer/wood-hammer.png), pointer`, deg: `rotate(deg: 25)` },
+        { url: `url(mouse-pointer/yellow-hammer.png), pointer`, deg: `rotate(deg: 0)` },
+    ];
 
     /** 1. 순차로 pointerNameList에서 css값 변수를 가져오는 함수
-     * @returns {string} 'url(mouse-pointer/simple-hammer.png), pointer;'
+     * @returns {object} {url, deg}
      * @author 'mimimya'
      */
     const getPointer = () => {
@@ -33,19 +33,15 @@ const changePointer = () => {
         return pointerNameList[changePointerCounter++];
     }
 
-    /**2.(사용 안하는 중)랜덤으로  pointerNameList에서 css값 변수를 가져오는 함수
-    * @returns {string} 'url(mouse-pointer/simple-hammer.png), pointer;'
-    * @author 'mimimya'
-    */
-    const getPointerRandom = () => {
-        return pointerNameList[Math.floor(Math.random() * pointerNameList.length)];
-    }
-
     //getPointer()-순차 OR getPointerRandom()-랜덤
 
     const val = getPointer();
     document.styleSheets[0].deleteRule(0);//생성할때 만들어진 CSS 객체를 삭제
-    document.styleSheets[0].insertRule(`html,button:hover{cursor : ${val}}`, 0); //다시 생성
+    document.styleSheets[0].insertRule( //다시 생성
+        `html,button:hover{
+            cursor : ${val.url};
+            transform: ${val.deg};
+        }`, 0);
     //document.styleSheets[0].cssRules[0].style.cursor = val;
     console.log(document.styleSheets[0].cssRules[0]);
     return;
