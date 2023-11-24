d7097456f5df259ff54f1bd11776d55379034bb7
eheh10 <lime1pm@naver.com>
2023-11-24 20:34:50 +0900

    MOVE js files -> js directory

---
+++ js/crack.js
@@ -0,0 +1,62 @@
+// 화면을 클릭할 때 실행되는 함수
+function insertImageOneClickCenter(event) {
+    // 클릭한 위치의 좌표를 가져옵니다.
+    const clickX = event.clientX;
+    const clickY = event.clientY;
+
+    // 이미지 엘리먼트를 생성하고 설정합니다.
+    const image = new Image();
+    image.src ='images/crack/cracked-glass-(1).png';
+
+    // 이미지의 크기를 설정합니다. 원하는 크기로 변경하세요.
+    const imageSize = 500; // 예: 이미지 크기를 50x50으로 설정
+
+    // 이미지의 중앙 위치 계산
+    const imageX = clickX - imageSize / 2 -90;
+    const imageY = clickY - imageSize / 2 -1;
+
+    // 이미지의 위치와 스타일을 설정합니다.
+    image.style.position = 'absolute';
+    image.style.left = `${imageX}px`;
+    image.style.top = `${imageY}px`;
+    image.style.width = `${imageSize}px`;
+    image.style.height = `${imageSize}px`;
+
+    image.className = 'crack-image';
+    
+    // 이미지를 body 요소에 추가합니다.
+    document.body.appendChild(image);
+}
+
+function insertImageTwoClickCenter(event) {
+    // 클릭한 위치의 좌표를 가져옵니다.
+    const clickX = event.clientX;
+    const clickY = event.clientY;
+
+    // 이미지 엘리먼트를 생성하고 설정합니다.
+    const image = new Image();
+    image.src ='images/crack/cracked-glass-(5).png';
+
+    // 이미지의 크기를 설정합니다. 원하는 크기로 변경하세요.
+    const imageSize = 500; // 예: 이미지 크기를 50x50으로 설정
+
+    // 이미지의 중앙 위치 계산
+    const imageX = clickX - imageSize / 2 -80;
+    const imageY = clickY - imageSize / 2 -40;
+
+    // 이미지의 위치와 스타일을 설정합니다.
+    image.style.position = 'absolute';
+    image.style.left = `${imageX}px`;
+    image.style.top = `${imageY}px`;
+    image.style.width = `${imageSize}px`;
+    image.style.height = `${imageSize}px`;
+
+    image.className = 'crack-image';
+
+    // 이미지를 body 요소에 추가합니다.
+    document.body.appendChild(image);
+}
+
+// 화면을 클릭할 때 insertImageAtClickCenter 함수를 호출합니다.
+document.addEventListener('click', insertImageOneClickCenter);
+document.addEventListener('dblclick', insertImageTwoClickCenter);
---
+++ js/screen.js
@@ -0,0 +1,24 @@
+/**
+ * 페이지 시작할 때 한번 배경이미지 랜덤으로 바꾸기
+**/
+
+const images = new Array();
+images[0]="images/block/block1.jpg";
+images[1]="images/block/block2.jpg";
+images[2]="images/block/block3.jpg";
+images[3]="images/code/code1.png";
+images[4]="images/code/code2.jpg";
+images[5]="images/glass/glass1.jpg";
+images[6]="images/glass/glass2.jpg";
+images[7]="images/glass/glass3.jpg";
+
+
+console.log(images);
+
+export function showImage() {
+    let imgNum = Math.floor(Math.random()*images.length);
+    const objImg = document.getElementById("introImg");
+    objImg.src = images[imgNum];
+}
+
+showImage();
