080d21b36779b1e6e35a65c275b6212e7bb4ed8e
eunhye park <lime1pm@naver.com>
2023-11-26 01:42:03 +0900

    Refactor button.js

--- js/button.js
+++ js/button.js
@@ -1,23 +1,9 @@
 import {showImage} from './screen.js';
 
-const ALLOW_FILE_EXTENSIONS = ['jpg', 'png'];
+const ALLOW_EXTENSIONS_LOWER_CASE = ['jpg', 'jpeg', 'png', 'gif'];
 
 const resetBtn = document.getElementById('reset-btn');
-const uploadBtn = document.getElementById('input-image');
-
-
-/**
- * crack image 제거
- */
-function removeCracks() {
-    const crackImages = document.getElementsByClassName("crack-image");
-
-    if (crackImages.length != 0) {
-        while(crackImages.length > 0) {
-            crackImages[0].remove();
-        }
-    }
-}
+const selectBtn = document.getElementById('input-image');
 
 
 /**
@@ -25,44 +11,76 @@ function removeCracks() {
  * crack 제거 및 랜덤으로 배경화면 변경
  */
 resetBtn.addEventListener('click', () => {
-    
+    // crack 제거
     removeCracks();
     
     // 배경화면 랜덤으로 변경
     showImage();
 });
 
-
 /**
- * UPLOAD 버튼 이미지 선택 이벤트핸들러
+ * SELECT 버튼 이미지 선택 이벤트핸들러
  * crack 제거 및 선택한 배경화면으로 변경
  */
-uploadBtn.onchange = () => {
-    const uploadFiles = uploadBtn.files;
-    
-    // 파일 수 예외처리
-    if (uploadFiles.length != 1) {
-        alert('하나의 이미지 파일을 업로드하세요');
-    }
+selectBtn.onchange = () => {
+    const selectFiles = selectBtn.files;
 
-    const imageFile = uploadFiles[0];
-    const fileType = imageFile.type;
-    const extension = fileType.split('/').pop().toLowerCase();
+    // 파일 예외처리
+    checkValidation(selectFiles);
+
+    const imageFile = selectFiles[0];
     
-    // 파일 확장자 예외처리
-    if (!ALLOW_FILE_EXTENSIONS.includes(extension)) {
-        alert(ALLOW_FILE_EXTENSIONS.join(" ") + ' 만 가능!');
+    // 배경화면 변경
+    changeBackground(imageFile);
+    
+    // crack 제거
+    removeCracks();
+}
+
+/**
+ * crack image 제거
+ */
+function removeCracks() {
+    const crackImages = document.getElementsByClassName("crack-image");
+    
+    if (crackImages.length === 0) {
         return;
     }
 
-    // 배경화면 변경
+    for(let idx=crackImages.length-1; idx >= 0 ; idx--) {
+        crackImages[idx].remove();
+    }
+}
+
+/**
+ * 파일 수, 확장자 유효성 검사
+ * @param {FileList} files 
+ */
+function checkValidation(files) {
+    // 파일 수 예외처리
+    if (files.length != 1) {
+        alert('하나의 이미지 파일을 선택하세요');
+    }
+ 
+    const imageFile = files[0];
+    const extension = imageFile.type.split('/').pop().toLowerCase();
+     
+    // 파일 확장자 예외처리
+    if (!ALLOW_EXTENSIONS_LOWER_CASE.includes(extension)) {
+        alert(ALLOW_EXTENSIONS_LOWER_CASE.join(",") + ' 만 가능!');
+    }
+}
+
+/**
+ * 이미지파일을 배경화면으로 변경
+ * @param {File} imageFile 
+ */
+function changeBackground(imageFile) {
     const fileReader = new FileReader();
-
+ 
     fileReader.readAsDataURL(imageFile);
-
+ 
     fileReader.onload = function (e) {
         document.getElementById('introImg').src = e.target.result;
-      };
-    
-      removeCracks();
+    };
 }
